4592775a1b50bb4ac90bbabdf8136624be22046d
Cathie Yun <cathieyun@gmail.com>
2013-12-03 01:33:47 -0500

    created models for Questions, Answers, Articles, Users

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from chesire.core.models import Answer
+from chesire.core.models import AnswerVote
+from chesire.core.models import Article
+from chesire.core.models import ChesireUser
+from chesire.core.models import Question
+from chesire.core.models import QuestionVote
+
+
+admin.site.register(Answer)
+admin.site.register(AnswerVote)
+admin.site.register(Article)
+admin.site.register(ChesireUser)
+admin.site.register(Question)
+admin.site.register(QuestionVote)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import User 
 from django.db import models
 
-# Create your models here.
+
+class ChesireUser(models.Model):
+    email = models.EmailField()
+    user = models.OneToOneField(User)
+
+class Vote(models.Model):
+    user = models.ForeignKey('ChesireUser')
+    VOTE_CHOICES = (
+        ('UP', 'Up'),
+        ('DOWN', 'Down'),
+    )
+    vote = models.CharField(choices=VOTE_CHOICES,
+                            max_length=4)
+    time = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        abstract = True
+
+class QuestionVote(Vote):
+    question = models.ForeignKey('Question')
+
+class AnswerVote(Vote):
+    answer = models.ForeignKey('Answer')
+
+class Question(models.Model):
+    text = models.TextField()
+    article = models.ForeignKey('Article')
+
+    def __unicode__(self):
+        return self.text
+
+class Answer(models.Model):
+    user = models.ForeignKey('ChesireUser')
+    text = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey('Question')
+
+    def __unicode__(self):
+        return self.text
+    
+class Article(models.Model):
+    url = models.URLField()
+    text = models.TextField()
+
